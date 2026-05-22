58226c642c02f80edc51f8119678e9344d2cf9cb
Trevor SANDY <trevor.sandy@gmail.com>
2026-05-23 00:58:21 +0200

    OpenClaw reorder setup updates

--- suite_services.py
+++ suite_services.py
@@ -2046,8 +2046,8 @@ def prepare_openclaw_env(environment, oc_store, oc_cwd):
     if environment == "public":
         _openclaw_compose_override()
     _openclaw_compose_updates()
-    _openclaw_clawdoc_updates()
     _openclaw_env_vars_logging()
+    _openclaw_clawdoc_updates()
     return True
 
 def prepare_openclaw_config(oc_cwd, env_vars):
